96190d731c452111ebfbe9f196bea188901bfa3d
josh137xd <112659525+josh137xd@users.noreply.github.com>
2022-12-12 16:48:17 -0500

    added animation and some js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const pochita = document.getElementById("pochita");
+const cone = document.getElementById("cone ");
 
-function
+function jump() {
+  if (pochita.classList != "jump") {
+    pochita.classList.add("jump");
+
+    setTimeout(function () {
+      pochita.classList.remove("jump");
+    }, 300);
+  }
+}
+
+   
+document.addEventListener("keydown", function (event) {
+  jump();
+});
