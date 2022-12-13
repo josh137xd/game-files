d0ae90c8f345a0d956df8687af8e9bf89d67b3f2
josh137xd <112659525+josh137xd@users.noreply.github.com>
2022-12-13 16:40:07 -0500

    added hit markers to game

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const pochita = document.getElementById("pochita");
-const cone = document.getElementById("cone ");
+const cone = document.getElementById("cone");
 // function jump to add jump delay
 function jump() {
   if (pochita.classList != "jump") {
@@ -20,14 +20,16 @@ document.addEventListener("keydown", function (event) {
 
 let isAlive = setInterval(function(){
 // pochita y position
-   let pochita = parseInt (window.getComputedStyle(pochita).getPropertyValue("top")
+   let pochitaTop = parseInt(window.getComputedStyle(pochita).getPropertyValue("top")
    );
    console.log(pochitaTop);
 // cone x position
-   let cone = parseInt(window.getComputedStyle(cone).getPropertyValue("left")
+   let coneLeft = parseInt(window.getComputedStyle(cone).getPropertyValue("left")
    );
 // hit markers
-
+   if (coneLeft <50 && coneLeft > 0 && pochitaTop >=430){
+   alert("GAME OVER")
+   }
 
 
 }, 10);
