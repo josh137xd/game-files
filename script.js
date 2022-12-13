ee68ac326a5c639105b94b85f3dfc9b47a1d20d8
josh137xd <112659525+josh137xd@users.noreply.github.com>
2022-12-13 15:57:36 -0500

    added y and x postion with js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const pochita = document.getElementById("pochita");
 const cone = document.getElementById("cone ");
-
+// function jump to add jump delay
 function jump() {
   if (pochita.classList != "jump") {
     pochita.classList.add("jump");
@@ -11,7 +11,23 @@ function jump() {
   }
 }
 
-   
+// keybinding jump 
 document.addEventListener("keydown", function (event) {
   jump();
 });
+
+//getting values from pochita and cone
+
+let isAlive = setInterval(function(){
+// pochita y position
+   let pochita = parseInt (window.getComputedStyle(pochita).getPropertyValue("top")
+   );
+   console.log(pochitaTop);
+// cone x position
+   let cone = parseInt(window.getComputedStyle(cone).getPropertyValue("left")
+   );
+// hit markers
+
+
+
+}, 10);
