09b8af30c49672386b9a953f18e719d861bdda73
josh137xd <112659525+josh137xd@users.noreply.github.com>
2022-12-20 16:56:10 -0500

    added final touches

--- script.js
+++ script.js
@@ -1,91 +1,41 @@
-import { updateGround, setupGround } from "./ground.js"
-import { updateDino, setupDino, getDinoRect, setDinoLose } from "./pochita.js"
-import { updateCactus, setupCactus, getCactusRects } from "./cone.js"
+let score = 0;
+const scoreDisplay = document.getElementById("score-display");
+const pochita = document.getElementById("img2");
+const cone = document.getElementById("img3");
 
-const WORLD_WIDTH = 100
-const WORLD_HEIGHT = 30
-const SPEED_SCALE_INCREASE = 0.00001
+// function jump to add jump delay
+function jump() {
+  if (pochita.classList != "jump") {
+    pochita.classList.add("jump");
 
-const worldElem = document.querySelector("[data-game]")
-const scoreElem = document.querySelector("[data-score]")
-const startScreenElem = document.querySelector("[data-start-screen]")
-
-setPixelToWorldScale()
-window.addEventListener("resize", setPixelToWorldScale)
-document.addEventListener("keydown", handleStart, { once: true })
-
-let lastTime
-let speedScale
-let score
-function update(time) {
-  if (lastTime == null) {
-    lastTime = time
-    window.requestAnimationFrame(update)
-    return
+    setTimeout(function () {
+      pochita.classList.remove("jump");
+    }, 300);
   }
-  const delta = time - lastTime
-
-  updateGround(delta, speedScale)
-  updatePochita(delta, speedScale)
-  updateCone(delta, speedScale)
-  updateSpeedScale(delta)
-  updateScore(delta)
-  if (checkLose()) return handleLose()
-
-  lastTime = time
-  window.requestAnimationFrame(update)
 }
+ 
+  
+// keybinding jump 
+document.addEventListener("keydown", function (event) {
+  jump();
+});
 
-function checkLose() {
-  const dinoRect = getDinoRect()
-  return getCactusRects().some(rect => isCollision(rect, dinoRect))
-}
+//getting values from pochita and cone
 
-function isCollision(rect1, rect2) {
-  return (
-    rect1.left < rect2.right &&
-    rect1.top < rect2.bottom &&
-    rect1.right > rect2.left &&
-    rect1.bottom > rect2.top
-  )
-}
+let isAlive = setInterval(function(){
+// pochita y position
+   let pochitaTop = parseInt(window.getComputedStyle(pochita).getPropertyValue("top")
+   );
+   console.log(pochitaTop);
+// cone x position
+   let coneLeft = parseInt(window.getComputedStyle(cone).getPropertyValue("left")
+   );
+// hit markers
+   if (coneLeft < 135 && coneLeft > 45 && pochitaTop >= 560){
+   alert("GAME OVER")
+   };
+  }, 10 );
+ 
 
-function updateSpeedScale(delta) {
-  speedScale += delta * SPEED_SCALE_INCREASE
-}
 
-function updateScore(delta) {
-  score += delta * 0.01
-  scoreElem.textContent = Math.floor(score)
-}
-
-function handleStart() {
-  lastTime = null
-  speedScale = 1
-  score = 0
-  setupGround()
-  setupDino()
-  setupCactus()
-  startScreenElem.classList.add("hide")
-  window.requestAnimationFrame(update)
-}
-
-function handleLose() {
-  setDinoLose()
-  setTimeout(() => {
-    document.addEventListener("keydown", handleStart, { once: true })
-    startScreenElem.classList.remove("hide")
-  }, 100)
-}
-
-function setPixelToWorldScale() {
-  let worldToPixelScale
-  if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
-    worldToPixelScale = window.innerWidth / WORLD_WIDTH
-  } else {
-    worldToPixelScale = window.innerHeight / WORLD_HEIGHT
-  }
-
-  worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
-  worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
-}
+  
